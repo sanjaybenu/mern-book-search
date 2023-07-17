33775e042d69cba6c451057b92a363ade839efa3
S Chopra <sanjaybenu@gmail.com>
2023-07-17 22:45:14 +1000

    added code

---
+++ client/src/utils/queries.js
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+// export const QUERY_USERS = gql`
+//   query allUsers {
+//     users {
+//       _id
+//       username
+//       email
+//       savedBooks {
+//         bookID
+//         authors
+//         description
+//         title
+//         image
+//         link
+//       }
+//     }
+//   }
+// `;
+
+// export const QUERY_SINGLE_USER = gql`
+//   query singleUser($userId: ID!) {
+//     user(userID: $userID) {
+//       _id
+//       username
+//       email
+//       savedBooks {
+//         bookID
+//         authors
+//         description
+//         title
+//         image
+//         link
+//       }
+//     }
+//   }
+// `;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,72 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate("savedBooks");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('savedBooks');
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id }).populate("savedBooks");
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No profile with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, {input}, context)=>{
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: { savedBooks: input },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+  removeBook: async (parent,{bookId},context)=> { 
+    if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks:{bookId: bookId }} },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    }
+  },
+};
+
+module.exports = resolvers;
